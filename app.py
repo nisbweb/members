86d0dd2766f5e619cf43cc931a20f43da2041d5a
mridulganga <mridul-ganga@hpe.com>
2019-10-08 12:29:02 +0530

    config travis

--- app.py
+++ app.py
@@ -9,57 +9,59 @@ CORS(app)
 
 def verify_auth(auth):
     r = requests.get("http://nisb-auth.herokuapp.com/auth?auth="+auth)
-    return r.status_code==200
+    return r.status_code == 200
 
 # Does nothing
 @app.route('/')
 def index():
     return jsonify({
-        "status":"ok"
+        "status": "ok"
     })
+
 
 @app.route("/members", methods=["GET"])
 def members_controller():
     if not verify_auth(request.args.get("auth")):
-        return jsonify({"status":"error","error":"auth is not valid"}),403
+        return jsonify({"status": "error", "error": "auth is not valid"}), 403
     members = get_members()
     if members:
         return jsonify(members)
     return jsonify({
-        "status":"error",
-        "error":"no member exists"
+        "status": "error",
+        "error": "no member exists"
     })
 
-@app.route("/member", methods=["GET","PUT","POST","DELETE"])
+
+@app.route("/member", methods=["GET", "PUT", "POST", "DELETE"])
 def member_controller():
     if not verify_auth(request.args.get("auth")):
-        return jsonify({"status":"error","error":"auth is not valid"}),403
-    if request.method=="GET":
+        return jsonify({"status": "error", "error": "auth is not valid"}), 403
+    if request.method == "GET":
         email = request.args.get("email")
         member = get_member(email)
         if member:
             return jsonify(member)
         else:
             return jsonify({
-                "status":"error",
-                "error":"member not found"
+                "status": "error",
+                "error": "member not found"
             })
 
-    elif request.method=="PUT":
+    elif request.method == "PUT":
         member_dict = request.get_json()
         update_member(member_dict)
-        return jsonify({"status":"ok"})
+        return jsonify({"status": "ok"})
 
-    elif request.method=="DELETE":
+    elif request.method == "DELETE":
         email = request.args.get("email")
         delete_member(email)
-        return jsonify({"status":"ok"})
+        return jsonify({"status": "ok"})
 
-    elif request.method=="POST":
+    elif request.method == "POST":
         member_dict = request.get_json()
         add_member(member_dict)
-        return jsonify({"status":"ok"})
-            
+        return jsonify({"status": "ok"})
+
 
 if __name__ == '__main__':
     # session["login"] = False
--- db.py
+++ db.py
@@ -1,8 +1,9 @@
 import pymongo
 import os
 
-client = pymongo.MongoClient(os.environ["MONGO"],connect=False)
+client = pymongo.MongoClient(os.environ["MONGO"], connect=False)
 db = client.main
+
 
 def get_members():
     members = []
@@ -10,24 +11,28 @@ def get_members():
     for m in ms:
         m.pop("_id")
         members.append(m)
-    if len(members)>0:
+    if len(members) > 0:
         return members
     else:
         return None
 
+
 def get_member(email):
-    member = db.members.find_one({"email":email})
+    member = db.members.find_one({"email": email})
     if member:
         member.pop("_id")
         return member
     return None
 
+
 def add_member(member_dict):
     db.members.insert_one(member_dict)
 
+
 def delete_member(email):
-    db.members.delete_one({"email":email})
+    db.members.delete_one({"email": email})
+
 
 def update_member(member_dict):
     email = member_dict["email"]
-    db.members.find_one_and_replace({"email":email},member_dict)
+    db.members.find_one_and_replace({"email": email}, member_dict)
